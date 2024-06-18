3b3ba58786345bd5a86685f0201ce8202ef1449c
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-06-18 20:37:03 -0300

    Add ajuste formula barsi

--- Finance/Controllers/CalculationOfTheBarsiController.cs
+++ Finance/Controllers/CalculationOfTheBarsiController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Finance.Web.Models;
+using Finance.Web.Service.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Finance.Web.Controllers
 {
     public class CalculationOfTheBarsiController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var result = new List<Barsi>();
+
+            var allStockExchangeService = new AllStockExchangeService();
+
+            var model = await allStockExchangeService.GetAllActiveAsync();
+
+            foreach (var item in model)
+            {
+                if (item.Price != 0)
+                {
+                    if (item.Financials.Dividends.Yield_12m != 0)
+                    {
+                        result.Add(new Barsi
+                        {
+                            Kind = item.Kind,
+                            Small = item.Logo.Small,
+                            Symbol = item.Symbol,
+                            Name = item.Name,
+                            CompanyName = item.CompanyName,
+                            Sector = item.Sector,
+                            Price = item.Price,
+                            Yield_12m = item.Financials.Dividends.Yield_12m_sum,
+                        });
+                    }
+                }
+            }
+
+            result = result.Select(p => new Barsi
+            {
+                Kind = p.Kind,
+                Small = p.Small,
+                Symbol = p.Symbol,
+                Name = p.Name,
+                CompanyName = p.CompanyName,
+                Sector = p.Sector,
+                Price = p.Price,
+                Yield_12m = p.Yield_12m,
+                Result = (p.Yield_12m / p.Price) * 100
+            }).OrderByDescending(p => p.Result).ToList();
+
+            return View(result);
         }
     }
 }
---
+++ Finance/Models/Barsi.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Finance.Web.Models
+{
+    public class Barsi
+    {
+        [JsonPropertyName("king")]
+        public string Kind { get; set; }
+        [JsonPropertyName("small")]
+        public string Small { get; set; }
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("company_name")]
+        public string CompanyName { get; set; }
+        [JsonPropertyName("sector")]
+        public string Sector { get; set; }
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+        [JsonPropertyName("change_percent")]
+        public decimal Yield_12m { get; set; }
+        [JsonPropertyName("result")]
+        public decimal Result { get; set; }
+
+    }
+}
